92c4be1fa9f58aff182b76845575823390c2ddb8
Kevinlatui <nganvuthu77@gmail.com>
2024-08-06 21:52:50 +0700

    ok

---
+++ hinh31.py
@@ -0,0 +1,22 @@
+import turtle
+
+window = turtle.Screen()
+window.title("Hinh31")
+window.bgcolor("white")
+
+pen = turtle.Turtle()
+pen.shape("turtle")
+pen.speed(5)
+
+pen.lt(60)
+def f():
+    pen.fd(50)
+    pen.lt(45)
+    pen.fd(100)
+    pen.lt(150)
+    pen.fd(100)
+    pen.lt(45)
+    pen.fd(50)
+    pen.lt(120)
+f()
+turtle.done()
---
+++ hinh32.py
@@ -0,0 +1,25 @@
+import turtle
+
+window = turtle.Screen()
+window.title("Hinh31")
+window.bgcolor("white")
+
+pen = turtle.Turtle()
+pen.shape("turtle")
+pen.speed(5)
+
+pen.lt(60)
+def f():
+    pen.fd(50)
+    pen.lt(45)
+    pen.fd(100)
+    pen.lt(150)
+    pen.fd(100)
+    pen.lt(45)
+    pen.fd(50)
+    pen.lt(120)
+    pen.rt(60)
+for _ in range(8):  
+    f()
+    pen.rt(360/8)
+turtle.done()
