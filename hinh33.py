ad2293d977c2033456ce8891ff59af2a25508ce2
Kevinlatui <nganvuthu77@gmail.com>
2024-08-07 10:06:03 +0700

    G

--- hinh33.py
+++ hinh33.py
@@ -9,26 +9,28 @@ pen.shape("classic")
 pen.speed(5)
 
 def g(circle):
+    circle = 30    
     for _ in range(4):
         pen.fd(10)
         pen.lt(180)
         pen.fd(10)
-        pen.rt(90)
-    for _ in range(4):
+        pen.rt(90) 
+    for _ in range(2):
         pen.penup()
         pen.fd(30)
         pen.lt(90)
         pen.pendown()
         pen.circle(circle)
+        circle = circle * 1.5  
         pen.penup()
         pen.fd(30)
         pen.pendown()
-        pen.lt(90)
-        pen.circle(circle*1.5)
-circle = 30
+        pen.lt(90)       
+        pen.circle(circle)
 
 
 
-g(circle)
+
+g(30)
 
 turtle.done()
