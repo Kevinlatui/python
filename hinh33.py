3362106c659697c2aaa6b328c88a34e4203a15ed
Kevinlatui <nganvuthu77@gmail.com>
2024-08-11 19:41:27 +0700

    v

--- hinh33.py
+++ hinh33.py
@@ -8,29 +8,42 @@ pen = turtle.Turtle()
 pen.shape("classic")
 pen.speed(5)
 
-def g(circle):
-    circle = 30    
-    for _ in range(4):
-        pen.fd(10)
-        pen.lt(180)
-        pen.fd(10)
-        pen.rt(90) 
-    for _ in range(2):
-        pen.penup()
-        pen.fd(30)
-        pen.lt(90)
-        pen.pendown()
-        pen.circle(circle)
-        circle = circle * 1.5  
-        pen.penup()
-        pen.fd(30)
-        pen.pendown()
-        pen.lt(90)       
-        pen.circle(circle)
 
-
-
-
-g(30)
-
+# circle = 30    
+for _ in range(4):
+    pen.fd(10)
+    pen.lt(180)
+    pen.fd(10)
+    pen.rt(90) 
+    
+def f(g):
+# for _ in range(2):
+    g = 30
+    pen.penup()
+    pen.fd(30)
+    pen.lt(90)
+    pen.pendown()
+    pen.circle(g)
+# circle = circle * 1.5 
+    pen.penup()
+    pen.rt(90)
+    pen.fd(10)
+    pen.left(90)
+    pen.pendown()
+# pen.lt(50)       
+    pen.circle(g+10)
+    pen.penup()
+    pen.right(90)
+    pen.forward(10)
+    pen.left(90)
+    pen.pendown()
+    pen.circle(g+20)
+    pen.penup()
+    pen.right(90)
+    pen.forward(10)
+    pen.left(90)
+    pen.pendown()
+    pen.circle(g+30)
+    
+f(30)
 turtle.done()
