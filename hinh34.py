86add2656604c115c54913d75f841cb158c4722b
Kevinlatui <nganvuthu77@gmail.com>
2024-08-11 20:51:50 +0700

    fygvy

---
+++ hinh34.py
@@ -0,0 +1,29 @@
+import turtle
+
+window = turtle.Screen()
+window.title("Hinh34")
+window.bgcolor("white")
+
+pen = turtle.Turtle()
+pen.shape("classic")
+pen.speed(5)
+
+def  s():
+    pen.circle(30)
+    pen.fd(100)
+    pen.lt(90)
+    pen.fd(100)
+    pen.circle(30)
+    pen.fd(100)
+    pen.lt(90)
+    pen.fd(100)
+    pen.circle(30)
+    pen.fd(100)
+    pen.lt(90)
+    pen.fd(100)
+    pen.circle(30)
+    pen.fd(100)
+    pen.lt(90)
+    pen.fd(100)
+s()
+turtle.done()
