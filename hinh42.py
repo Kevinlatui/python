ffaf16ed17e2b083096cb0a1125e6e9ab6d2eca9
Kevinlatui <nganvuthu77@gmail.com>
2024-08-19 09:32:13 +0700

    f

---
+++ hinh42.py
@@ -0,0 +1,18 @@
+import turtle
+
+window = turtle.Screen()
+window.title("Hinh43")
+window.bgcolor("white")
+
+pen = turtle.Turtle()
+pen.shape("classic")
+pen.speed(15)
+
+def w():
+    pen.circle(30)
+    pen.lt(50)
+    pen.lt(14)
+for _ in range(20):
+    w()
+    pen.lt(360/20)
+turtle.done()
