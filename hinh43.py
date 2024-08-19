bf983dd556586712b3c21df1276b683783860a44
Kevinlatui <nganvuthu77@gmail.com>
2024-08-19 10:32:13 +0700

    sf

--- hinh43.py
+++ hinh43.py
@@ -6,15 +6,12 @@ window.bgcolor("white")
 
 pen = turtle.Turtle()
 pen.shape("classic")
-pen.speed(5)
+pen.speed(1)
 
-def w():
-    pen.rt(90)
-    pen.circle(30)
-    pen.lt(50)
-    pen.lt(14)
 for _ in range(20):
-    w()
+    pen.circle(30)
+    # pen.lt(50)
+    # pen.lt(14)
     pen.lt(360/20)
 turtle.done()
 
